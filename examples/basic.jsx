d65cdb97612e59834ca25042f06071fff7952093
Andrew Russell <russell@idelictech.com>
2017-03-19 19:31:11 -0400

    Added a basic form example.

---
+++ examples/basic.jsx
@@ -0,0 +1,68 @@
+import Immutable from 'immutable';
+import React from 'react';
+
+import {create, Form, Types} from 'formatron';
+
+const exampleSchema = create('example', {
+  data: [
+    Types.data.text.create('title', {
+      required: true,
+      textType: 'singleline'
+    }),
+    Types.data.number.create('count', {
+      required: true,
+      numberType: 'integer'
+    }),
+    Types.data.text.create('nested', {}, ['path', 'to'])
+  ],
+  form: Types.form.columns.create({
+    label: 'Example Form',
+    columns: [[
+      Types.form.data.create({
+        label: 'Title',
+        ref: 'title'
+      }),
+      Types.form.data.create({
+        label: 'Count',
+        ref: 'count'
+      }),
+      Types.form.data.create({
+        label: 'Nested',
+        ref: 'nested'
+      })
+    ]]
+  })
+});
+
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      model: Immutable.fromJS({
+        title: 'Default Title',
+        path: {
+          to: {
+            value: ''
+          }
+        ]
+      })
+    };
+  }
+
+  onSubmit(newModel) {
+    this.setState({model: newModel});
+  }
+
+  render() {
+    return <Form
+      schema={exampleSchema}
+      model={this.state.model}
+      onSubmit={this.onSubmit}
+    >
+      <button>Submit</button>
+    </Form>;
+  }
+}
+
