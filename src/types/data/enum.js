24831f5daac034dd715e07b63bd62ad73f8ceb06
Andrew Russell <russell@idelictech.com>
2017-03-20 09:28:48 -0400

    Updated enum type to allow multi-selects.

--- src/types/data/enum.js
+++ src/types/data/enum.js
@@ -1,83 +1,162 @@
-import Immutable, {List, Map} from 'immutable';
+import {List, Map} from 'immutable';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import Select from 'react-select';
 
 import {validationErrors} from './';
 
-export default function (register) {
+import 'react-select/dist/react-select.css';
+
+export default function(register) {
   register('enum', {
     parseOptions(options) {
       return options
         .update('values', values => List(values)
-          .map(value => Map(value))
+          .map(value => typeof value == 'string' ?
+            Map({value, label: value}) :
+            Map(value)
+          )
         );
     },
+
     component: EnumComponent,
     validate: validateEnum,
-    toString: enumToString
+    toString: enumToString,
+    getDefaultValue: getDefaultEnumValue
   });
 }
 
-const EnumComponent = ({name, value, options, disabled, onChange, onBlur}) => {
+const EnumComponent = (props) => {
+  if (props.options.get('multi')) {
+    return <MultiEnumComponent {...props} />;
+  } else {
+    return <SingleEnumComponent {...props} />;
+  }
+};
+
+const MultiEnumComponent = ({name, value, options, disabled, onChange, onBlur}) => {
   return <Select
-    className='form-data-input select single'
+    className='form-data-input select multi'
     name={name}
-    value={valueToOption(value)}
+    value={multiValueToOptions(value)}
     disabled={disabled}
-    options={options.get('values')
-      .unshift({
-        label: '',
-        value: null
-      })
-      .toJS()
-    }
-    onChange={option => onChange(optionToValue(option))}
+    multi={true}
+    options={getSelectOptions(options)}
+    onChange={options => onChange(optionsToMultiValue(options))}
     onBlur={onBlur}
   />;
 };
 
-EnumComponent.propTypes = {
+MultiEnumComponent.propTypes = {
   name: React.PropTypes.string.isRequired,
-  value: ImmutablePropTypes.contains({
-    value: React.PropTypes.number.isRequired
-  }),
+  value: React.PropTypes.oneOfType([
+    ImmutablePropTypes.listOf(
+      React.PropTypes.string.isRequired
+    ).isRequired,
+    ImmutablePropTypes.listOf(
+      React.PropTypes.number.isRequired
+    ).isRequired
+  ]).isRequired,
   options: ImmutablePropTypes.map.isRequired,
   disabled: React.PropTypes.bool.isRequired,
   onChange: React.PropTypes.func.isRequired,
   onBlur: React.PropTypes.func.isRequired
 };
 
-function valueToOption(value) {
+function multiValueToOptions(value) {
   return value ?
-    value.get('value') :
-    '';
+    value.toJS() :
+    [];
 }
 
-function optionToValue(option) {
+function optionsToMultiValue(options) {
+  return List(options)
+    .map(optionToSingleValue)
+    .filter(option => option);
+}
+
+const SingleEnumComponent = ({name, value, options, disabled, onChange, onBlur}) => {
+  return <Select
+    className='form-data-input select single'
+    name={name}
+    value={singleValueToOption(value)}
+    disabled={disabled}
+    options={getSelectOptions(options)}
+    onChange={option => onChange(optionToSingleValue(option))}
+    onBlur={onBlur}
+  />;
+};
+
+SingleEnumComponent.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  value: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.number
+  ]),
+  options: ImmutablePropTypes.map.isRequired,
+  disabled: React.PropTypes.bool.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+  onBlur: React.PropTypes.func.isRequired
+};
+
+function singleValueToOption(value) {
+  if (value === null) {
+    return '';
+  }
+  return value;
+}
+
+function optionToSingleValue(option) {
   if (!option || typeof option.value == 'undefined' || option.value === null) {
     return null;
   }
-  return Map({
-    value: option.value
-  });
+  return option.value;
+}
+
+function getSelectOptions(options) {
+  return options.get('values', List());
+}
+
+function getDefaultEnumValue(options) {
+  if (options.get('multi')) {
+    return List();
+  } else {
+    return null;
+  }
 }
 
 function enumToString(value, options) {
-  const option = options.get('value')
-    .find(option => option.get('value') == value.get('value'));
+  if (options.get('multi')) {
+    const selectOptions = getSelectOptions(options)
+      .filter(option => value.includes(option.get('value')));
 
-  if (option) {
-    return option.get('label');
+    return selectOptions
+      .map(option => option.get('label'))
+      .join(', ');
+  } else {
+    const option = getSelectOptions(options)
+      .find(option => option.get('value') == value);
+
+    return option ?
+      option.get('label') :
+      '';
   }
-  return '';
 }
 
 function validateEnum(value, options) {
-  const option = options.get('values')
-    .find(option => option.get('value') == value.get('value'));
+  if (options.get('multi')) {
+    const selectOptions = getSelectOptions(options)
+      .filter(option => value.includes(option.get('value')));
 
-  if (!option) {
-    throw new Error(validationErrors.invalidOption);
+    if (value.size != selectOptions.size) {
+      throw new Error(validationErrors.invalidOption);
+    }
+  } else {
+    const option = getSelectOptions(options)
+      .find(option => option.get('value') == value);
+
+    if (!option) {
+      throw new Error(validationErrors.invalidOption);
+    }
   }
 }
 
